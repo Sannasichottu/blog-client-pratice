47b35e46f59d9f8f66c623b782272dfe125fa11b
Sannasi <sannasichottu@gmail.com>
2023-01-20 14:49:03 +0530

    crud

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -9,6 +9,38 @@ export const getPosts = () => async (dispatch) => {
     } catch (error) {
         console.log(error.message);
     }
+}
+
+//Create Post
+export const createPost = (post)=> async (dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+
+        dispatch ({type:'CREATE', payload:data})
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
+//update post
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+        const {data} = await api.updatePost(id,post);
+
+        dispatch({type:'UPDATE',payload:data})
+     } catch (error) {
+         console.log(error.message)
+     }
+}
+
+//delete post
+export const deletePost  = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+
+        dispatch({ type : 'DELETE', payload:id})
+    } catch (error) {
+        console.log(error);
+    }
 }

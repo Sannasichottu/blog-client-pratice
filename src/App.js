8c2e1a7c098bc92bdecc2155def87656d8d206ff
Sannasi <sannasichottu@gmail.com>
2023-01-20 20:47:12 +0530

    navbar-setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React,{useState, useEffect} from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import {getPosts} from './actions/posts';
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts'
 import useStyles from './styles'
+import Navbar from './components/Navbar/Navbar';
 
-import memories from './components/images/memories.png';
 
 
 const App = () => {
@@ -20,12 +20,7 @@ const App = () => {
 
   return (
    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position='static' color='inherit' >
-        <Typography className={classes.heading} variant='h3' align='center' >
-          Memories
-        </Typography>
-        <img className={classes.image} src={memories} alt="icon" height="50" />
-      </AppBar>
+      <Navbar />
       <Grow in >
         <Grid className={classes.mainContainer} container justify='space-between' alignItems='stretch' spacing={3} >
           <Grid item xs={12} sm={7}>
